8c828ce7b842dc5f0b47d47035e885dcb5caa3f4
Alexis-Dk <alexeydruzik@inbox.ru>
2020-08-25 14:02:53 +0300

    Refactoring.

--- src/com/BPSSberbank/serviceDesk/Main.java
+++ src/com/BPSSberbank/serviceDesk/Main.java
@@ -13,14 +13,17 @@ public class Main {
 
     private final static String[] words = new String[] {"Саша", "Паша", "Нина", "Леша", "Игорь", "Максим", "Ольга"};
     private final static String[] addition_words = new String[] {"Никон", "Афанасий", "Серафим"};
+    private static final int TIME_SLEEP_FOR_FIRST_PRODUCER = 5000;
+    private static final int TIME_SLEEP_FOR_SECOND_PRODUCER = 6000;
+    private static final int TIME_SLEEP_FOR_FIRST_CONSUMER = 8000;
 
     public static void main(String[] args) {
 
         SimpleQueueImpl<String> queue = new SimpleQueueImpl();
 
-        new Producer(queue, words, 5000);
-        new Producer(queue, addition_words, 6000);
-        new Consumer(queue, 8000);
+        new Producer(queue, words, TIME_SLEEP_FOR_FIRST_PRODUCER);
+        new Producer(queue, addition_words, TIME_SLEEP_FOR_SECOND_PRODUCER);
+        new Consumer(queue, TIME_SLEEP_FOR_FIRST_CONSUMER);
 
     }
 
